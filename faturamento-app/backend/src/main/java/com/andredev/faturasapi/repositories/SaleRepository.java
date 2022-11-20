bb5c12f27d694bf2c3e6ea3f11f1aa588341e429
andre06x <andre06x@gmail.com>
2022-11-20 18:48:18 -0300

    alterando order by para data

--- faturamento-app/backend/src/main/java/com/andredev/faturasapi/repositories/SaleRepository.java
+++ faturamento-app/backend/src/main/java/com/andredev/faturasapi/repositories/SaleRepository.java
@@ -9,6 +9,6 @@ import org.springframework.data.jpa.repository.Query;
 import java.time.LocalDate;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
-    @Query("SELECT obj FROM Sale obj WHERE obj.date BETWEEN :min AND :max ORDER BY obj.amount DESC")
+    @Query("SELECT obj FROM Sale obj WHERE obj.date BETWEEN :min AND :max ORDER BY obj.date DESC")
     Page<Sale> findSales(LocalDate min, LocalDate max, Pageable pageable);
 }
